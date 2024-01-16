37808a3fcc2a090e6b281346e15ade2edfbae78d
Monroebu <monroebu@student.21-school.ru>
2024-01-16 13:23:06 +0500

    исправила путь в build.sh!!!

--- src/05/build.sh
+++ src/05/build.sh
@@ -4,6 +4,6 @@ sudo docker build -t my_docker .
 
 #sudo docker run -d -p 80:81 --name my_container my_docker
 
-sudo docker run -d -p 80:81 --name my_container -v /home/monroebu/DO5_SimpleDocker-1/src/04/nginx/nginx.conf:/etc/nginx/nginx.conf -d my_docker
+sudo docker run -d -p 80:81 --name my_container -v /home/monroebu/github/DO5_SimpleDocker-1_github/src/05/nginx/nginx.conf:/etc/nginx/nginx.conf -d my_docker
 
 echo -e "\033[32m""\t\tmonroebu""\033[0m"
